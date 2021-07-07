d76916140afbbf4fda97da551d2e0e7ea389ede8
samadii <77415087+samadii@users.noreply.github.com>
2021-07-07 14:11:16 +0430

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,2 @@
+welcome_text = 'Hi!\nWelcome to Optical Character Recognizer Bot.\nJust send a clear image to me and I will recognize the text in the image and send it as a message!'
+reply_to_text_message = 'Hey, I\'m not designed to reply to text messages 😕\nHit /start and read the instructions!'
